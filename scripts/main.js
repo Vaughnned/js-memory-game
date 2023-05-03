772be648df3e4d0f4415a281f52239994f49de58
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-03 18:49:19 -0400

    mostly css changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,28 +3,28 @@
 
   const cards = [
     {
-      text: "Apple",
-      url: "https://freepngimg.com/save/133395-slice-apple-download-free-image/512x367",
+      text: "10",
+      url: "https://previews.123rf.com/images/henningmarquardt/henningmarquardt1703/henningmarquardt170300277/74712994-large-index-playing-card-10-of-diamonds.jpg",
     },
     {
-      text: "Banana",
-      url: "https://png.pngtree.com/png-clipart/20220716/ourmid/pngtree-banana-yellow-fruit-banana-skewers-png-image_5944324.png",
+      text: "Jack",
+      url: "https://banner2.cleanpng.com/20180323/ecw/kisspng-jack-playing-card-spades-valet-de-pique-card-game-cards-5ab597d7866db6.8504334215218503275506.jpg",
     },
     {
-      text: "Orange",
-      url: "https://pngimg.com/d/orange_PNG780.png",
+      text: "Queen",
+      url: "https://w7.pngwing.com/pngs/154/214/png-transparent-queen-of-hearts-playing-card-graphy-playing-cards-white-candle-natural.png",
     },
     {
-      text: "Peach",
-      url: "https://purepng.com/public/uploads/large/purepng.com-peachpeachfruitfullwholetwopeachessweetfoodorange-4815216318294z3v2.png",
+      text: "King",
+      url: "https://i.pinimg.com/originals/05/42/bf/0542bf18697e6304dbe4b84278d05994.png",
     },
     {
-      text: "Blueberries",
-      url: "https://pngimg.com/d/blueberries_PNG16.png",
+      text: "Ace",
+      url: "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/74850/ace-of-diamonds-clipart-xl.png",
     },
     {
-      text: "Blackberries",
-      url: "https://www.transparentpng.com/thumb/blackberry-fruit/purple-blackberry-fruit-png-free-download-raspberry-black-mulberry-5SyhoM.png",
+      text: "Joker",
+      url: "https://media.istockphoto.com/id/1153852267/vector/playing-card-joker-yellow-red-blue-black.jpg?s=612x612&w=0&k=20&c=Hgz0isA2JMM-q6zOSND372GmFTFzP6HZKYD_tVWK5rk=",
     },
   ];
 
@@ -40,12 +40,12 @@
       const card = deck[i];
       html += `
         <div class="card" data-name=${card.text}>
-            <div class="card__side card-back">
-                <img src="https://clockworkchilli.com/assets/demos/memory/images/back.png" alt="Card Back" /> 
-            </div>
-            <div class="card__side card-front">
+            <div class="card__face card__face--front">
                 <img src=${card.url} alt=${card.text} />
             </div>
+            <div class="card__face card__face--back">
+            <img src="https://i.pinimg.com/originals/72/13/e1/7213e1a55a3c0c73d5864e14ba1aa7dc.jpg" alt="Card Back" /> 
+        </div>
         </div>
         `;
     }
@@ -61,6 +61,10 @@
     }
   }
 
+  //   function disableCard() {
+  //    flippedCards.removeEventListener("click", play());
+  //   }
+
   let flippedCards = [];
   let matchedCards = [];
 
@@ -71,8 +75,10 @@
       flippedCards = [];
       console.log(flippedCards);
       console.log(matchedCards);
+      //   disableCard();
     } else {
       flippedCards = [];
+      //   event.currentTarget.classList.add("flip");
       console.log(flippedCards);
     }
   }
@@ -87,6 +93,9 @@
     if (matchedCards.length === 12) {
       matchedCards = [];
     }
+    // if (checkForMatch() !== true) {
+    //   event.currentTarget.classList.remove("flip");
+    // }
   }
 
   function play() {
