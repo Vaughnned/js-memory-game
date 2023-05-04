23582b46af05150e6f41595cfd191cbab1c30228
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-04 17:25:18 -0400

    lots of changes and refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -28,67 +28,106 @@
     },
   ];
 
+  const deck = [...cards, ...cards]; // add more ...cards if you want a larger memory game
+  const htmlContainer = document.querySelector(".container");
+  let flippedCards = [],
+    matchedCards = [];
+  console.log({ matchedCards });
+
   function shuffleCards(arr) {
     return arr.sort(function () {
       return Math.random() - 0.5;
     });
   }
 
-  function generateCardsHTML(deck) {
+  shuffleCards(deck);
+  generateCardsHTML(deck, htmlContainer);
+
+  function generateCardsHTML(deck, container) {
     let html = "";
     for (let i = 0; i < deck.length; i++) {
       const card = deck[i];
       html += `
-        <div class="card" data-name=${card.text}>
+        <div class="card" data-name="${card.text}">
             <div class="card__face card__face--front">
-                <img src=${card.url} alt=${card.text} />
+                <img src="${card.url}" alt="${card.text}" />
             </div>
             <div class="card__face card__face--back">
-            <img src="https://i.pinimg.com/originals/72/13/e1/7213e1a55a3c0c73d5864e14ba1aa7dc.jpg" alt="Card Back" /> 
+            <img class="card__image" src="https://i.pinimg.com/originals/72/13/e1/7213e1a55a3c0c73d5864e14ba1aa7dc.jpg" alt="Card Back" /> 
         </div>
         </div>
         `;
     }
-    return html;
+    container.innerHTML = "";
+    container.insertAdjacentHTML("afterbegin", html);
   }
 
   function checkForDuplicate() {
+    console.log({ card1: flippedCards[0] });
+    console.log({ card2: flippedCards[1] });
     if (
-      matchedCards.includes(flippedCards[0]) &&
-      matchedCards.includes(flippedCards[1])
+      matchedCards.includes(flippedCards[0])
+      // && matchedCards.includes(flippedCards[1].dataset.name)
     ) {
+      console.log(matchedCards.includes(flippedCards[0]));
       return true;
     }
+    return false;
   }
 
-  //   function disableCard() {
-  //    flippedCards.removeEventListener("click", play());
-  //   }
-
-  let flippedCards = [];
-  let matchedCards = [];
-
-  function checkForMatch() {
-    if (flippedCards[0] === flippedCards[1] && checkForDuplicate() !== true) {
-      matchedCards.push(flippedCards[0]);
-      matchedCards.push(flippedCards[1]);
-      flippedCards = [];
-      console.log(flippedCards);
-      console.log(matchedCards);
-      //   disableCard();
-    } else {
-      flippedCards = [];
-      //   event.currentTarget.classList.add("flip");
-      console.log(flippedCards);
+  function disableCards() {
+    flippedCards.forEach(function (card) {
+      card.removeEventListener("click", flipCard);
+      card.classList.add("match");
+      matchedCards.push(card);
+    });
+    flippedCards = [];
+    if (matchedCards.length === 12) {
+      setTimeout(function () {
+        alert("Congrats! Play Again?"), 500;
+      });
     }
   }
 
-  function flipCard(event) {
-    event.currentTarget.classList.add("flip");
-    flippedCards.push(event.currentTarget.dataset.name);
+  function checkForMatch() {
+    console.dir({ flippedCards });
+    console.log("checkForMatch firing");
+    if (
+      flippedCards[0].dataset.name === flippedCards[1].dataset.name &&
+      checkForDuplicate() === false
+    ) {
+      matchedCards.push(flippedCards[0]);
+      matchedCards.push(flippedCards[1]);
+
+      console.log({ flippedCards });
+      console.log({ matchedCards });
+      disableCards();
+    } else {
+      unflipCards();
+      console.log({ flippedCards });
+    }
+    flippedCards = [];
+  }
+
+  function unflipCards() {
+    flippedCards.forEach(function (card) {
+      setTimeout(function () {
+        card.classList.remove("flip");
+      }, 1000);
+    });
+    flippedCards = [];
+  }
+
+  function flipCard() {
+    if (flippedCards[0] === this) {
+      return;
+    }
+    this.classList.add("flip");
+    flippedCards.push(this);
 
     if (flippedCards.length === 2) {
       checkForMatch();
+      flippedCards = [];
     }
     if (matchedCards.length === 12) {
       matchedCards = [];
@@ -99,12 +138,10 @@
   }
 
   function play() {
-    const deck = [...cards, ...cards];
-    shuffleCards(deck);
+    // shuffleCards(deck);
 
-    const htmlContainer = document.querySelector(".container");
-    const cardsHtml = generateCardsHTML(deck);
-    htmlContainer.insertAdjacentHTML("afterbegin", cardsHtml);
+    // const cardsHtml = generateCardsHTML(deck, htmlContainer);
+    // htmlContainer.insertAdjacentHTML("afterbegin", cardsHtml);
 
     const cardNodes = document.querySelectorAll(".card");
     for (let i = 0; i < cardNodes.length; i++) {
